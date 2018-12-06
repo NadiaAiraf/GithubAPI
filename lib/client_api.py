b492086e10abafac4436d3b6431b1b0be26ddd45
NadiaAiraf <aidanfaria@gmail.com>
2018-12-06 14:47:53 +0000

    renamed api interaction class, returns array of all languages now

---
+++ lib/client_api.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+class ClientApi:
+
+    def __init__(self):
+        self.response = None
+
+    def return_repo_languages(self, username):
+        return return_languages(make_api_request(username))
+
+    def make_api_request(self, username):
+        url = "https://api.github.com/users/" + username + "/repos"
+        self.response = requests.get(url)
+        return self.response.text
+
+    def return_languages(self, data):
+        repositories = json.loads(data)
+        languages = []
+        for repo in repositories:
+            languages.append(repo['language'])
+        return languages
---
+++ test/test_client_api.py
@@ -0,0 +1,25 @@
+import unittest
+from unittest.mock import Mock, patch
+from nose.tools import assert_is_not_none
+from lib.client_api import ClientApi
+
+class Interaction(unittest.TestCase):
+
+    def test_make_api_request(mock_get):
+        client_api = ClientApi()
+        with patch('lib.client_api.requests.get') as mock_get:
+            mock_get.return_value_ok = True
+            response = client_api.make_api_request('nadiaairaf')
+        assert_is_not_none(response)
+
+    def test_return_languages(self):
+        client_api = ClientApi()
+        json_string = '[{"language":"Javascript"}]'
+        language = client_api.return_languages(json_string)
+        self.assertEqual(language, ['Javascript'])
+
+    def test_return_languages_multiple(self):
+        client_api = ClientApi()
+        json_string = '[{"language":"Javascript"}, {"language": "Ruby"}]'
+        language = client_api.return_languages(json_string)
+        self.assertEqual(language, ['Javascript', 'Ruby'])
