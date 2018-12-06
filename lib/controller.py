d6b33a33f5c5046fb59257187f723fae4df44b95
NadiaAiraf <aidanfaria@gmail.com>
2018-12-06 15:58:46 +0000

    fixed controller variables needing to be instances of Display/ClientApi classes

--- lib/controller.py
+++ lib/controller.py
@@ -1,12 +1,13 @@
-from lib.display import *
+from lib.display import Display
 from lib.client_api import *
 
 class Controller:
 
     def __init__(self, display=Display, client_api=ClientApi):
-        self.display = display
-        self.client_api = ClientApi
+        self.display = display()
+        self.client_api = client_api()
 
     def run_app(self):
         username = self.display.get_search_word()
         languages = self.client_api.return_repo_languages(username)
+        return languages
