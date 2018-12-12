063606b0631e5f91005f91b6d0c7529663472dab
NadiaAiraf <aidanfaria@gmail.com>
2018-12-12 15:11:38 +0000

    testing, creation of mocks has finally happened, wohoo

--- lib/controller.py
+++ lib/controller.py
@@ -3,9 +3,9 @@ from lib.client_api import *
 
 class Controller:
 
-    def __init__(self, display=Display, client_api=ClientApi):
-        self.display = display()
-        self.client_api = client_api()
+    def __init__(self, display=Display(), client_api=ClientApi()):
+        self.display = display
+        self.client_api = client_api
 
 
     def favourite_language(self, array):
@@ -14,5 +14,6 @@ class Controller:
     def run_app(self):
         username = self.display.get_search_word()
         languages = self.client_api.return_repo_languages(username)
+        # print(self.client_api)
         favourite_language = self.favourite_language(languages)
         self.display.return_favourite_language(favourite_language)
--- test/test_controller.py
+++ test/test_controller.py
@@ -5,14 +5,18 @@ from lib.controller import *
 class ControllerTest(unittest.TestCase):
 
     def setUp(self):
-        client_api_mock = mock.Mock()
-        display_mock = mock.Mock()
-        self.controller = Controller(display_mock, client_api_mock)
+        self.client_api_mock = mock.MagicMock()
+        self.client_api_mock.return_repo_languages.return_value = 'nadiaairaf'
 
-    # def test_run_app(self):
-    #     self.controller.main()
-    #     self.controller.display.get_search_word.assert_called()
-    #     self.controller.client_api_mock.get_search_word.assert_called()
+        self.display_mock = mock.MagicMock()
+        self.display_mock.get_search_word.return_value = ['python', 'ruby', 'python']
+        self.controller = Controller(self.display_mock, self.client_api_mock)
+
+    def test_run_app(self):
+        self.controller.run_app()
+        self.controller.display.get_search_word.assert_called()
+        self.controller.client_api.return_repo_languages.assert_called()
+        self.controller.display.return_favourite_language.assert_called()
 
     def test_favourite_language(self):
         array_of_languages = ['ruby', 'python', 'python', 'javascript']
